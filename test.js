0447dea73a183604fedab3acc6ca46bdb02d1702
renaissancedog <123657188+renaissancedog@users.noreply.github.com>
2023-01-27 19:26:21 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,54 +1,12 @@
-var cookie = createSprite(200, 200);
-var score=0;
-var roundscore=0;
-var debug=false;
-var mouse=0;
-var buymouse = createSprite(350, 50);
-var mousecost=15;
-var roundmousecost=15;
-buymouse.setAnimation("blank");
-buymouse.debug=debug;
+// Get the clicker button and score elements
+const clicker = document.getElementById("clicker");
+const score = document.getElementById("score");
 
-var grandma=0;
-var buygrandma = createSprite(350, 150);
-var grandmacost=100;
-var roundgrandmacost=100;
-buygrandma.setAnimation("blank");
-buygrandma.debug=debug;
+// Set the initial score to 0
+let currentScore = 0;
 
-World.frameRate=60;
-function draw() {
-  background("white");
-
-  text(roundscore+" cookies", 0, 15);
-  text("You have "+mouse+" cursors. Buy a mouse for "+roundmousecost + " cookies.", 300, 10, 100, 100);
-  text("You have "+grandma+" grandmas. Buy a grandma for "+roundgrandmacost + " cookies.", 300, 100, 100, 100);
-
-  cookie.setAnimation("cookie");
-  cookie.scale = 0.4;
-  drawSprites();
-  
-  if (mouseWentDown("leftButton")&&mouseIsOver(cookie)) {
-    score+=1;
-    console.log(score);
-    
-  }
-  if (mouseWentDown("leftButton")&&mouseIsOver(buymouse)&&(score>=mousecost)) {
-    score-=mousecost;
-    mousecost*=1.15;
-    roundmousecost=(Math.round(mousecost));
-    mouse+=1;
-    
-  }
-  if (mouseWentDown("leftButton")&&mouseIsOver(buygrandma)&&(score>=grandmacost)) {
-    score-=grandmacost;
-    grandmacost*=1.15;
-    roundgrandmacost=(Math.round(grandmacost));
-    grandma+=1;
-    
-  }
-  
-  score=score+mouse/300;
-  roundscore=Math.floor(score);
-  console.log(roundscore);
-}
+// Update the score when the button is clicked
+clicker.addEventListener("click", function() {
+  currentScore += 1;
+  score.innerHTML = currentScore;
+});
