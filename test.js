4a85bd675d15848a108e05140ab2555336c219b2
renaissancedog <123657188+renaissancedog@users.noreply.github.com>
2023-01-27 19:13:36 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,35 +1,54 @@
 var cookie = createSprite(200, 200);
 var score=0;
 var roundscore=0;
-var cursor=0;
-var time=0;
-var seconds=0;
-var buymouse = createSprite(300, 150);
-buymouse.setAnimation("buymouse");
+var debug=false;
+var mouse=0;
+var buymouse = createSprite(350, 50);
+var mousecost=15;
+var roundmousecost=15;
+buymouse.setAnimation("blank");
+buymouse.debug=debug;
+
+var grandma=0;
+var buygrandma = createSprite(350, 150);
+var grandmacost=100;
+var roundgrandmacost=100;
+buygrandma.setAnimation("blank");
+buygrandma.debug=debug;
 
 World.frameRate=60;
 function draw() {
-  time+=1;
-  seconds=Math.round(time/60);
   background("white");
+
+  text(roundscore+" cookies", 0, 15);
+  text("You have "+mouse+" cursors. Buy a mouse for "+roundmousecost + " cookies.", 300, 10, 100, 100);
+  text("You have "+grandma+" grandmas. Buy a grandma for "+roundgrandmacost + " cookies.", 300, 100, 100, 100);
+
   cookie.setAnimation("cookie");
   cookie.scale = 0.4;
   drawSprites();
-  text(roundscore+" cookies", 0, 15);
-  text(seconds+" seconds", 0, 105);
-  text("buy mouse", 300, 150);
+  
   if (mouseWentDown("leftButton")&&mouseIsOver(cookie)) {
     score+=1;
     console.log(score);
     
   }
-  if (mouseWentDown("leftButton")&&mouseIsOver(buymouse)&&(score>=15)) {
-    score-=15;
-    cursor+=1;
+  if (mouseWentDown("leftButton")&&mouseIsOver(buymouse)&&(score>=mousecost)) {
+    score-=mousecost;
+    mousecost*=1.15;
+    roundmousecost=(Math.round(mousecost));
+    mouse+=1;
+    
+  }
+  if (mouseWentDown("leftButton")&&mouseIsOver(buygrandma)&&(score>=grandmacost)) {
+    score-=grandmacost;
+    grandmacost*=1.15;
+    roundgrandmacost=(Math.round(grandmacost));
+    grandma+=1;
     
   }
   
-  score=score+cursor/300;
+  score=score+mouse/300;
   roundscore=Math.floor(score);
   console.log(roundscore);
 }
